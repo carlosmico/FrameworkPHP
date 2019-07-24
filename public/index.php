807e931cbf71270ce110bbb21deb7ea529ccd1d7
Carlos Mico <carlosmico15@gmail.com>
2019-07-24 10:49:24 +0200

    Autoload.php

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+//Cargamos el archivo autoload para poder tener configurado el namespace de App (src)
+require_once dirname(__DIR__).'/vendor/autoload.php';
+
+use App\Kernel;
+
+$kernel = new Kernel();
